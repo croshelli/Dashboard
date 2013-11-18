5ed102fd8ece4f6f7bc172c60ba3e9d3d08fb969
ajamxb <ajalma08@gmail.com>
2013-11-18 11:17:41 -0500

    There is some scaling now when you hover over an icon. It's too big but we can make it smaller later.

--- home.js
+++ home.js
@@ -158,8 +158,8 @@ function displayImage(id, file, xPos, yPos, imgWidth, imgHeight, link) {
 				.attr("x", xPos)
 				.attr("y", yPos)
 				.attr("width", imgWidth)
-				.attr("height", imgHeight);
-				/*.on("mouseover", function(d) {
+				.attr("height", imgHeight)
+				.on("mouseover", function(d) {
 					var x = -xPos - imgWidth / 2;
 					var y = -yPos - imgHeight / 2;
 					var xCenter = xPos + imgWidth / 2;
@@ -170,17 +170,17 @@ function displayImage(id, file, xPos, yPos, imgWidth, imgHeight, link) {
 					var yNew = yCenter - imgHeight * 1.2; 
 					
 					d3.select(this)
-						.attr("transform", "translate(" + x + "," + y + "), scale(1.2, 1.2), translate(" + (xScaled + imgWidth * 1.2 / 2 + xNew) + "," + (yScaled + imgHeight * 1.2 / 2 + yNew) + ")");
-						//.attr("transform", "translate(" + x + "," + y + "), scale(1.5, 1.5), translate(" + -(x - x / 1.5) + "," + -(y - y / 1.5) + ")");
+						//.attr("transform", "translate(" + x + "," + y + "), scale(1.2, 1.2), translate(" + (xScaled + imgWidth * 1.2 / 2 + xNew) + "," + (yScaled + imgHeight * 1.2 / 2 + yNew) + ")");
+						.attr("transform", "translate(" + x + "," + y + "), scale(1.5, 1.5), translate(" + -(x - x / 1.5) + "," + -(y - y / 1.5) + ")");
+				})
+				.on("mouseout", function(d) {
+					d3.select(this)
+					var x = -xPos - imgWidth *1.5 / 2;
+					var y = -yPos - imgHeight * 1.5/ 2; 
+					d3.select(this)
+						//.attr("transform", "translate(" + x + "," + y + "), scale(1.0, 1.0), translate(" + (- x  - imgWidth  / 2) + "," + (-y  - imgHeight  2)+ ")");
+						.attr("transform", "translate(" + x + "," + y + "), scale(1.0, 1.0), translate(" + -x + "," + -y + ")");
 				});
-				/*.on("mouseout", function(d) {
-					d3.select(this)
-					var x = -xPos - imgWidth / 2;
-					var y = -yPos - imgHeight / 2; 
-					d3.select(this)
-						.attr("transform", "translate(" + x + "," + y + "), scale(1.0, 1.0), translate(" + (- x  - imgWidth  / 2) + "," + (-y  - imgHeight  2)+ ")");
-						//.attr("transform", "translate(" + x + "," + y + "), scale(1.5, 1.5), translate(" + -(x - x / 1.5) + "," + -(y - y / 1.5) + ")");
-				});*/
 	
 }
 
