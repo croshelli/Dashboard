308cdb5ca36b813c79d1b970b1d58a3917774672
Annette Almonte <ajalma08@gmail.com>
2013-11-26 18:35:12 -0500

    Deleted methods that initially made the banner and navigation bar.

--- home.js
+++ home.js
@@ -28,8 +28,6 @@ function initialize() {
             console.log(data);  
             tabsData = data;
             setLayout();
-            //displayBanner();
-            //generateTabs();
             setImages();
 
         }
@@ -37,22 +35,6 @@ function initialize() {
 }
 
 function setLayout() {
- 	
-	/*banner = d3.select("body")
-				.append("svg")
-				.attr("class", "head")
-				.attr("id", "banner")
-				.attr("width", headWidth)
-				.attr("height", headerHeight)
-				.attr("overflow", "visible");
-				
-	menu = d3.select("body")
-				.append("svg")
-				.attr("class", "head")
-				.attr("id", "svgmenu")
-				.attr("width", headWidth)
-				.attr("height", menuHeight)
-				.attr("overflow", "visible");*/	
 				
 	imageCanvas = d3.select("body")
  					.append("svg")
@@ -60,83 +42,6 @@ function setLayout() {
  					.attr("width", headWidth)
  					.attr("height", imageCanvasHeight);	
 }    
-
-function generateTabs() {
-	tabs = menu.selectAll("g")
-				.data(tabsData)
-				.enter()
-				.append("a")
-				.attr("xlink:href", function(d) {
-					return d.url;
-				})
-				.append("g")
-				.attr("class", function(d) {
-					return d.class;
-				})
-				.attr("x", function(d) {
-        			return d.x_position;
-        		})
-        		.attr("y", 0)
-		        .attr("width", function(d) {
-		        	return d.tab_width;
-		        })
-		        .attr("height", menuHeight)
-		        .on("mouseover", function(d) {
-        			d3.select(this).select("rect")
-        				.attr("fill", "#5d088a");	
-        		})
-        		.on("click", function(d) {
-        			d3.selectAll(".tab")
-        				.attr("fill", "#7709b2");
-        			d3.select(this).select(".tab")
-        				.attr("fill", "#5d088a");	
-        		})
-       		 	.on("mouseout", function(d) {
-        			d3.select(this).select("rect")
-        				.attr("fill", "#7709b2");	
-        		});
-	
-	tabs.append("rect")
-        .attr("x", function(d) {
-        	return d.x_position;
-        })
-        .attr("y", 0)
-        .attr("width", function(d) {
-        	return d.tab_width;
-        })
-        .attr("height", menuHeight)
-        .attr("fill", "#7709b2");
-  
-  
-  
-	var xOffset = 20;
-	var yOffset = 6;
-	        
-    tabs.append("text")
-        .attr("x", function(d, i){
-			return Number(d.x_position) + xOffset;
-		})
-		.attr("y", menuHeight / 2 + yOffset)
-        .attr("text-anchor", "start")
-        .text(function(d) { 
-        	return d.name; 
-        })
-        .attr("font-family", "Arial")
-        .attr("font-size", "21px")
-        .attr("font-weight", "bold")
-        .attr("fill", "white");	
-           
-}
-
-function displayBanner() {
-	
-	banner.append("image")
-			.attr("xlink:href", "home_images/banner.png")
-			.attr("x", 0)
-			.attr("y", 0)
-			.attr("height", 100)
-			.attr("width", 1100);
-}
 
 function setImages() {	
 	displayImage("plane", "home_images/plane.png", 625, 30, 238, 48, "aviation.html");
